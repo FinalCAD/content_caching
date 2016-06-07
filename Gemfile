71c08464b4f59bbb6a5aba27cbdda447325cd67d
Lukasz Sarnacki <lukesarnacki@gmail.com>
2016-06-07 10:52:12 +0200

    create directory if doesn't exist

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 gemspec
 
 group :test do
+  gem 'byebug'
   gem 'pry'
   gem 'configatron'
 end
--- lib/content_caching/adapters/fs.rb
+++ lib/content_caching/adapters/fs.rb
@@ -1,9 +1,12 @@
+require 'fileutils'
+
 module ContentCaching
   module Adapter
     class Fs
 
       def store document_path, content
         content.rewind if content.respond_to?(:rewind)
+        create_directory_for(document_path)
         File.write url(document_path), content.respond_to?(:read) ? content.read : content
       end
 
@@ -15,6 +18,14 @@ module ContentCaching
         File.delete url(document_path)
       end
 
+      private
+
+      def create_directory_for(document_path)
+        dir = File.dirname(document_path)
+        unless File.directory?(dir)
+          FileUtils.mkdir_p(dir)
+        end
+      end
     end
   end
 end
